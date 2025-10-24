042dc1988de52003d3643f197ee53fdbffe4c60e
quliyev223 <mikayilquliev@gmail.com>
2025-10-24 16:53:33 +0000

    task

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[46:61] +" "+ str[62:73]+ str[74:78]+" "+str[0:6])
+str = ("Python is an interpreted, interactive, object-oriented programming "
+       "language that combines remarkable power with very clear syntax")
+print(str[46:61] + " " + str[62:73] + " " + str[74:78] + " " + str[0:6])
 print(str)
