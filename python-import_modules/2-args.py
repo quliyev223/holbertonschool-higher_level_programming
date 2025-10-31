40d8038fc7bdc048fd7bd5cc910564d31dce49bd
quliyev223 <mikayilquliev@gmail.com>
2025-10-31 14:25:51 +0000

    task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     a = "arguments"
     b = ":"
-elif len(sys.argv) == 1:
+elif len(sys.argv) == 2:
     a = "argument"
     b = ":"
-if len(sys.argv) == 0:
+if len(sys.argv) == 1:
     b = "."
 print("{} {} {}".format(len(sys.argv), a, b))
 
