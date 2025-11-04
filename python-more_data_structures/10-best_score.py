e5c27a59824e7d23879c52ff99e9ae2c3b95598b
quliyev223 <mikayilquliev@gmail.com>
2025-11-04 14:46:10 +0000

    task

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     best_key = None
-    best_value = float('inf')
+    best_value = float('-inf')
     for key, value in a_dictionary.items():
         if value > best_value:
             best_value = value
