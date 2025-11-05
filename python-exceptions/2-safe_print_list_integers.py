84bc5e61300881ae8a4285703025c39a6c8fa2f2
quliyev223 <mikayilquliev@gmail.com>
2025-11-05 07:37:23 +0000

    task

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        except TypeError:
+            continue
