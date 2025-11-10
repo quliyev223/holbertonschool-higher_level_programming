8541a79012152d9c27bad8c6a29930f954dde6a4
quliyev223 <mikayilquliev@gmail.com>
2025-11-10 07:43:55 +0000

    task

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module contains the function text_indentation:
+prints a text with 2 new lines after '.', '?' and ':'
+"""
 def text_indentation(text):
     """Prints a text with 2 new lines after '.', '?' and ':'"""
     if not isinstance(text, str):
