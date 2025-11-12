d5cb50be9bc7ca118a0c2458ba9b8bba5edad229
quliyev223 <mikayilquliev@gmail.com>
2025-11-12 17:29:26 +0000

    task

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -18,3 +18,15 @@ class Square:
 
         Raises:
             TypeError: If `size` type is not `int`.
+
+            ValueError: If `size` is less than `0`.
+
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
