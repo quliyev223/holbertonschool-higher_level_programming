a80ce040f4eb44e3932929d3669b45edf06faa60
quliyev223 <mikayilquliev@gmail.com>
2025-11-12 17:28:34 +0000

    task

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""Square Class
+
+A Square Class
+
+"""
+
 
 class Square:
-    """class Square that defines a square
-
-        Attributes:
-        attr1 (size): Size of the Square.
-
-    """
 
     def __init__(self, size=0):
-        """Initializer with default size = 0"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """__init__
+
+        The __init__ method initializes the size value of the square.
+
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+
+        Raises:
+            TypeError: If `size` type is not `int`.
