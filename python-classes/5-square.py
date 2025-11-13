0890ee5e1d287a55eabdf90dec5b8bab0eed5dba
quliyev223 <mikayilquliev@gmail.com>
2025-11-13 07:13:23 +0000

    task

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -40,13 +40,12 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Prints the square with the character #.
-
-        If size is 0, prints an empty line.
-        """
+        """Prints the square with the character #."""
         if self.__size == 0:
-            print("")
+            print()
         else:
             for i in range(self.__size):
-                print("#" * self.__size)
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
 
