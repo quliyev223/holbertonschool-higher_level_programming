edbfae5525917769312396b77d5b467e7c81942e
quliyev223 <mikayilquliev@gmail.com>
2025-11-13 07:06:21 +0000

    task

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""
+1-square.py: is a class Square that defines a square
+"""
 
 
 class Square:
-    """Represents a square."""
+    """class Square that defines a square
+
+        Attributes:
+        attr1 (size): Size of the Square.
+
+    """
 
     def __init__(self, size=0):
-         """Initialize a new Square.
-         Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
-
-    @property
-    def size(self):
-        """Get the current size of the square."""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-         """Set the size of the square with validation."""
-         if not isinstance(value, int):
-             raise typeError("size must be an integer")
-         if value < 0:
-             raise ValueError("size must be >+ 0")
-         self.__size = value
+        """Initializer with default size = 0"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
-        return self.__size ** 2
+        """Returns area of square"""
+        return self.__size * self.__size
