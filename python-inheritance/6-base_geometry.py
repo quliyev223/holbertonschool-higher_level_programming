19c67d3dbba3eea6849943da6f63d9d649b89af5
quliyev223 <mikayilquliev@gmail.com>
2025-11-21 14:12:32 +0000

    task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+         """
+        Public instance method that is not implemented
+        """
+        raise Exception("area() is not implemented")
