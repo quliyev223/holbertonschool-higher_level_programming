109b5a546284a73ed25d063ae145cd4fb8ac89c2
quliyev223 <mikayilquliev@gmail.com>
2025-11-21 17:11:03 +0000

    task

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+import math
+
+
+#Abstract base class Shape
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        """Return the area of the shape"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Return the perimeter of the shape"""
+        pass
+
+
+# Rectangle class, inherits from Shape
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        # Area formula: width * height
+        return self.width * self.height
+
+    def perimeter(self):
+        # Perimeter formula: 2 * (width + height)
+        return 2 * (self.width + self.height)
+
+
+# Circle class, inherits from Shape
+class Circle(Shape):
+    def __init(self, radius):
+        self.radius = radius
+
+    def area(self):
+        # Area formula: p * r^2
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        #Perimeter formuls: 2 * p * r
+        return 2 * math.pi * self.radius
+
+
+# Function using duck typing to print shape info 
+def shape_info(shape):
+    # We assume the object has area() and perimeter methods
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perometer()}")
+
+# Testing the classes and function
+if __name == "__main__"
+    circle = Circle(radius=5)
+    rectangle = Rectangle(width=4, height=7)
+
+
+    shape_info(circle)
+    shape_info(rectangle)
