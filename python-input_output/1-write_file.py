78d52e5a7a3f37a9a6abf8bb1aa6197db7cd4abc
quliyev223 <mikayilquliev@gmail.com>
2025-11-24 06:42:09 +0000

    task

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-This module provides a function to write text to a file in UTF-8 encoding.
+"""This module provides a function to write text to a file in UTF-8 encoding.
 The file will be created if it does not exist, and overwritten if it does.
 """
 
 
 def write_file(filename="", text=""):
-     """
-    Writes a string to a UTF-8 encoded text file and returns
+    """Writes a string to a UTF-8 encoded text file and returns
     the number of characters written.
 
     Args:
