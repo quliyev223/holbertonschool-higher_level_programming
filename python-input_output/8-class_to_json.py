62b531ad8a3b501401f75a7acac12487097d0771
quliyev223 <mikayilquliev@gmail.com>
2025-11-27 06:18:11 +0000

    task

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Function that returns a dictionary description for JSON serialization."""
 
+
 def class_to_json(obj):
     """Return a dictionary of simple data structure attributes of obj."""
     return obj.__dict__.copy()
