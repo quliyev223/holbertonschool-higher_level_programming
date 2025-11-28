7589cef108bf44becc75a5a54bcb368deebcade0
quliyev223 <mikayilquliev@gmail.com>
2025-11-28 08:35:12 +0000

    task

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
+"""Defines class and serializes it"""
 import pickle
 
 
 class CustomObject:
-    """class displays attributes
+    """Class that represents a custom object with attributes:
     Args:
         name: string
         age: integer
         is_student: boolean
     """
     def __init__(self, name, age, is_student):
+        """Initializes the CustomObject instance"""
         self.name = name
         self.age = age
-        self.is_student
+        self.is_student = is_student
 
     def display(self):
+        """Prints out the attributes of the object"""
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """Serializes the current instance and saves it to a file."""
+        """Serializes the current instance of the object to a file"""
         try:
-            with open(filename, 'wb') as f
+            with open(filename, "wb") as f:
                 pickle.dump(self, f)
-        except:
+        except (IOError, pickle.PicklingError) as e:
+            print(f"Error during serialization: {e}")
             return None
-
 
     @classmethod
     def deserialize(cls, filename):
-        """method returns instance of custom object"""
+        """Deserializes an object from a file and returns an instance"""
         try:
             with open(filename, "rb") as f:
-                ret = pickle.load(f)
-                return ret
-        except:
+                return pickle.load(f)
+        except (FileNotFoundError, pickle.UnpicklingError, IOError) as e:
+            print(f"Error during deserialization: {e}")
             return None
-
-         
-
