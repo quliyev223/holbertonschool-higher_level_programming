baa26c7af68154e308041e465cf63c8a06940b92
quliyev223 <mikayilquliev@gmail.com>
2025-12-02 07:29:21 +0000

    task

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -2,4 +2,4 @@
 -- Deletes every record in second_table where the score is 5 or lower
 -- Only uses the score field (no id, no name needed)
 
-DELETE FRPM second_table WHERE score <= 5;
+DELETE FROM second_table WHERE score <= 5;
