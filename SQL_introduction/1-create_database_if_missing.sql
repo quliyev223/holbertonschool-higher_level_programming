afd06b87076511497b3bcd84aec7d757b584c9cf
quliyev223 <mikayilquliev@gmail.com>
2025-12-02 06:26:05 +0000

    task

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- This script creates the database hbtn_0c_0
+-- It will NOT give an error if the database already exists
+-- We do NOT use SELECT or SHOW (forbidden)
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
