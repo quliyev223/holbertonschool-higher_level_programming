fc707722986168f7cecd2174f8dcf0d9967a0227
quliyev223 <mikayilquliev@gmail.com>
2025-12-02 06:29:25 +0000

    task

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This script deletes the database hbtn_0c_0
+-- It will NOT give an error if the database does NOT exist
+-- We do NOT use SELECT or SHOW (forbidden)
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
