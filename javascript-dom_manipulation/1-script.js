d5ed876f1ce8e7ccc86e3f042ca7c1ea3e25b4b8
quliyev223 <mikayilquliev@gmail.com>
2025-12-26 08:07:35 +0000

    task

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,3 +1,29 @@
+// Write a JavaScript script that updates the text color of the header element to red (#FF0000)
+// when the user clicks on the tag with id "red_header".
+//
+// You must use document.querySelector
+//
+// Test with this HTML file:
+//
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <title>Holberton School</title>
+//   </head>
+//   <body>
+//     <header>
+//       First HTML page
+//     </header>
+//     <div id="red_header">Red header</div>
+//     <footer>
+//       Holberton School - 2022
+//     </footer>
+//     <script type="text/javascript" src="1-script.js"></script>
+//   </body>
+// </html>
+
+// Select the element with id "red_header" and add a click event listener
 document.querySelector('#red_header').addEventListener('click', function () {
+  // Change the text color of the <header> element to red
   document.querySelector('header').style.color = '#FF0000';
 });
