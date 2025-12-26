4288db62f7413d9a8c6fa21726a6d67c81f90f60
quliyev223 <mikayilquliev@gmail.com>
2025-12-26 08:04:25 +0000

    task

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,1 +1,22 @@
+// Write a JavaScript script that updates the text color of the header element to red (#FF0000)
+// You must use document.querySelector
+//
+// Test with this HTML file:
+//
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <title>Holberton School</title>
+//   </head>
+//   <body>
+//     <header>
+//       First HTML page
+//     </header>
+//     <footer>
+//       Holberton School - 2022
+//     </footer>
+//     <script type="text/javascript" src="0-script.js"></script>
+//   </body>
+// </html>
+
 document.querySelector('header').style.color = '#FF0000';
